4732f061cdd639eab22a555defffbacdeb4807b3
Juanjo <juanjogarau@gmail.com>
2020-10-18 21:36:13 -0400

    Started creating the algorithm structure
    Started creating the algorithm structure

--- lpg.py
+++ lpg.py
@@ -1,51 +1,121 @@
 import numpy as np
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.distributions.categorical import Categorical
+from torch.optim import Adam
+import argparse
+import time
+
+from networks import MetaLearnerNetwork, EmbeddingNetwork, Agent
+from utils import ParameterBandit, combined_shape, discount_cumsum
 
 
-class ParameterBandit:
+class DataBuffer:
 
-    def __init__(self, env_dist, temp=0.1, exp_bonus=0.2):
-        self.environments = [env.name for env in env_dist]
-        self.combinations = {env.name: [(lr, kl_cost) for lr in env.lrs for kl_cost in env.kl_costs]
-                             for env in env_dist}
-        self.returns = {(env_name, lr, kl_cost): [] for env_name in self.environments
-                        for (lr, kl_cost) in self.combinations[env_name]}
-        self.trials = {(env_name, lr, kl_cost): 0 for env_name in self.environments
-                       for (lr, kl_cost) in self.combinations[env_name]}
-        self.temp = temp
-        self.exp_bonus = exp_bonus
+    def __init__(self, obs_dim, act_dim, size, gamma=0.99):
+        self.obs_buf = np.zeros(combined_shape(size, obs_dim), dtype=np.float32)
+        self.act_buf = np.zeros(combined_shape(size, act_dim), dtype=np.float32)
+        self.rew_buf = np.zeros(size, dtype=np.float32)
+        self.ret_buf = np.zeros(size, dtype=np.float32)
+        self.logp_buf = np.zeros(size, dtype=np.float32)
+        self.ptr, self.path_start_idx, self.max_size = 0, 0, size
+        self.gamma = gamma
 
-    def sample_combination(self):
-        env_name = np.random.choice(self.environments)
-        logits = []
-        for (lr, kl_cost) in self.combinations[env_name]:
-            comb = (env_name, lr, kl_cost)
-            logit = (np.mean(self.returns[comb]) + self.exp_bonus / np.sqrt(self.trials[comb])) / self.temp
-            logits.append(logit)
-        logits = np.exp(logits)
-        logits = logits / np.sum(logits)
-        index = np.random.choice(len(self.combinations[env_name]), p=logits)
-        return env_name, self.combinations[env_name][index]
+    def store(self, obs, act, rew, logp):
 
-    def update_bandits_list(self, env_name_list, env_comb_list, ret_list):
-        for env_name, env_comb, ret in zip(env_name_list, env_comb_list, ret_list):
-            self.update_bandits(env_name, env_comb, ret)
+        assert self.ptr < self.max_size
+        self.obs_buf[self.ptr] = obs
+        self.act_buf[self.ptr] = act
+        self.rew_buf[self.ptr] = rew
+        self.logp_buf[self.ptr] = logp
+        self.ptr += 1
 
-    def update_bandits(self, env_name, env_comb, ret):
-        comb = (env_name, *env_comb)
-        self.returns[comb] = [ret] + self.returns[comb][:9]
-        self.trials[comb] += 1
+    def finish_path(self):
+
+        path_slice = slice(self.path_start_idx, self.ptr)
+        rews = self.rew_buf[path_slice]
+        self.ret_buf[path_slice] = discount_cumsum(rews, self.gamma)[:-1]
+        self.path_start_idx = self.ptr
+
+    def get(self):
+
+        assert self.ptr == self.max_size
+        self.ptr, self.path_start_idx = 0, 0
+        data = dict(obs=self.obs_buf, act=self.act_buf, ret=self.ret_buf,
+                    logp=self.logp_buf)
+        return {k: torch.as_tensor(v, dtype=torch.float32) for k, v in data.items()}
 
 
-def train_lpg(env_dist, init_agent_param_dist, num_parallel_lifetimes=1):
+def train_agent(env, lr, kl_cost, lifetime=1e6):
 
+    # Get environment parameters
+    obs_dim = env.observation_space.shape
+    act_dim = env.action_space.shape
+
+    # Agent network
+    agent = Agent(observation_space=env.observation_space, action_space=env.action_space, m=args.m)
+
+    # Set up optimizer for agent
+    agent_optim = Adam(agent.pi.parameters(), lr=lr)
+
+    # Set up data buffer
+    buf = DataBuffer(obs_dim=obs_dim, act_dim=act_dim, size=args.trajectory_steps, gamma=args.gamma)
+
+    # Update function
+    def update():
+        pass
+
+    # Prepare for interaction with environment
+    start_time = time.time()
+    o, ep_ret, ep_len = env.reset(), 0, 0
+
+    # Main loop: collect experience in env
+    for t in range(int(lifetime)):
+
+        # Agent take action
+        a, logp = agent.step(torch.as_tensor(o, dtype=torch.float32))
+
+        # Environment next step
+        next_o, r, d, _ = env.step(a)
+        ep_ret += r
+        ep_len += 1
+
+        # Save in buffer
+        buf.store(o, a, r, logp)
+
+        # Update obs
+        o = next_o
+
+        # Resetting the episode if current ended
+        if d:
+            buf.finish_path()
+            o, ep_ret, ep_len = env.reset(), 0, 0
+
+        if (t + 1) % args.trajectory_steps == 0:
+            update()
+
+
+def train_lpg(env_dist, init_agent_param_dist, num_parallel_lifetimes=1, seed=0):
+
+    env_dist_obs_dim = 16
+
+    # Random seed
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    # Create parameter bandit
     parameter_bandit = ParameterBandit(env_dist)
 
+    # Meta and Embedding networks
+    meta_net = MetaLearnerNetwork(inp_dim=env_dist_obs_dim, hidden_size=args.lstm_hidden_size, out_dim=2)
+    embed_net = EmbeddingNetwork(y_dim=args.m)
+
+    # Set up optimizer for Meta and Embedding
+    meta_optim = Adam(meta_net.parameters(), lr=args.meta_lr)
+    embed_optim = Adam(embed_net.parameters(), lr=0.001)
+
     for lifetime in range(num_parallel_lifetimes):
+
         env_name, (lr, kl_cost) = parameter_bandit.sample_combination()
+        env = env_dist.create(env_name)
 
 
 def lpg():
@@ -55,4 +125,14 @@ def lpg():
 
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Main script for running LPG")
+    parser.add_argument('--lstm_hidden_size', type=int, default=256)
+    parser.add_argument('--m', type=int, default=30)
+    parser.add_argument('--meta_lr', type=float, default=0.0001)
+    parser.add_argument('--train_pi_iters', type=int, default=5)
+    parser.add_argument('--trajectory_steps', type=int, default=20)
+    parser.add_argument('--gamma', type=float, default=0.995)
+    args = parser.parse_args()
+
     lpg()
